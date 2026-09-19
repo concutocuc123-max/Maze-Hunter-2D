0ed53a6d9d0bb5c7e6c34ea8ded3e3723d4681ab
concutocuc123-max <concutocuc123@gmail.com>
2026-09-19 20:43:23 +0700

    add gamemanager and playercontroller

---
+++ Assets/code/GameManager.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int score = 0;
+    public int health = 3;
+    public float timeRemaining = 60f;
+    public int coinsRequiredToPass = 10;
+    private int currentCoins = 0;
+
+    [Header("UI Elements")]
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI timerText;
+    public GameObject winPanel;
+    public GameObject losePanel;
+
+    private bool isGameOver = false;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
+
+    void Start()
+    {
+        winPanel.SetActive(false);
+        losePanel.SetActive(false);
+        UpdateUI();
+    }
+
+    void Update()
+    {
+        if (isGameOver) return;
+
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            UpdateUI();
+        }
+        else
+        {
+            GameOver(false); // Hết giờ -> Thua
+        }
+    }
+
+    public void AddCoin()
+    {
+        score += 10;
+        currentCoins++;
+        UpdateUI();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        UpdateUI();
+        if (health <= 0)
+        {
+            GameOver(false); // Hết máu -> Thua
+        }
+    }
+
+    public bool CanExit()
+    {
+        return currentCoins >= coinsRequiredToPass;
+    }
+
+    public void GameOver(bool isWin)
+    {
+        isGameOver = true;
+        Time.timeScale = 0; // Dừng game
+        if (isWin) winPanel.SetActive(true);
+        else losePanel.SetActive(true);
+    }
+
+    private void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+        healthText.text = "Health: " + health;
+        timerText.text = "Time: " + Mathf.CeilToInt(timeRemaining).ToString() + "s";
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextLevel()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/code/PlayerController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    
+    public AudioClip coinSound;
+    public AudioClip damageSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0; 
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            GameManager.Instance.AddCoin();
+            if(coinSound) audioSource.PlayOneShot(coinSound);
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("ExitDoor"))
+        {
+            if (GameManager.Instance.CanExit())
+            {
+                GameManager.Instance.GameOver(true); // Thắng
+            }
+            else
+            {
+                Debug.Log("Chưa thu thập đủ Coin!");
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Trap"))
+        {
+            GameManager.Instance.TakeDamage(1);
+            if(damageSound) audioSource.PlayOneShot(damageSound);
+            
+            Vector2 knockback = (transform.position - collision.transform.position).normalized;
+            rb.AddForce(knockback * 10f, ForceMode2D.Impulse);
+        }
+    }
+}
