73b7c93a0f35c5ceea4148cd0e7c8f98a630ef4e
concutocuc123-max <concutocuc123@gmail.com>
2026-09-19 21:12:06 +0700

    fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
-    
+    private SpriteRenderer spriteRenderer;
+
     public AudioClip coinSound;
     public AudioClip damageSound;
     private AudioSource audioSource;
@@ -16,7 +16,10 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 0; 
+        rb.gravityScale = 0; // Dùng cho Top-Down
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         audioSource = gameObject.AddComponent<AudioSource>();
     }
 
@@ -24,6 +27,15 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x > 0)
+        {
+            spriteRenderer.flipX = false; 
+        }
+        else if (movement.x < 0)
+        {
+            spriteRenderer.flipX = true; 
+        }
     }
 
     void FixedUpdate()
@@ -36,14 +48,14 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Coin"))
         {
             GameManager.Instance.AddCoin();
-            if(coinSound) audioSource.PlayOneShot(coinSound);
+            if (coinSound) audioSource.PlayOneShot(coinSound);
             Destroy(other.gameObject);
         }
         else if (other.CompareTag("ExitDoor"))
         {
             if (GameManager.Instance.CanExit())
             {
-                GameManager.Instance.GameOver(true); // Thắng
+                GameManager.Instance.GameOver(true);
             }
             else
             {
@@ -57,8 +69,9 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Trap"))
         {
             GameManager.Instance.TakeDamage(1);
-            if(damageSound) audioSource.PlayOneShot(damageSound);
+            if (damageSound) audioSource.PlayOneShot(damageSound);
             
+            // Đẩy lùi nhẹ (Knockback)
             Vector2 knockback = (transform.position - collision.transform.position).normalized;
             rb.AddForce(knockback * 10f, ForceMode2D.Impulse);
         }
